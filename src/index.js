0cae367f5389f0650b02c3ea92634634f02adf29
mitrofin <43621297+mitrofin@users.noreply.github.com>
2021-04-04 12:52:03 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './styles.css';
+import colors from "./templates/colors.json";
+
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+
+const onStart = document.querySelector('button[data-action="start"]');
+const onStop = document.querySelector('button[data-action="stop"]');
+
+let timerId = null;
