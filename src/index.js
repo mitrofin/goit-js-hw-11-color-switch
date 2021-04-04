84169b5c65dde76a8dadd87636282cdf2774fd69
mitrofin <43621297+mitrofin@users.noreply.github.com>
2021-04-04 18:30:02 +0300

    done

--- src/index.js
+++ src/index.js
@@ -11,3 +11,22 @@ const onStart = document.querySelector('button[data-action="start"]');
 const onStop = document.querySelector('button[data-action="stop"]');
 
 let timerId = null;
+
+onStart.addEventListener("click", onStartClick);
+onStop.addEventListener('click', onStopClick);
+console.log(onStart);
+
+function onStartClick() {
+  if (onStart.disabled) {
+    return
+  }
+    onStart.disabled = true;
+    timerId = setInterval(() => {
+        document.body.style.background = colors[randomIntegerFromInterval(0, colors.length - 1)];
+    }, 1000);
+}
+
+function onStopClick() {
+    onStart.disabled = false;
+    clearInterval(timerId);
+}
